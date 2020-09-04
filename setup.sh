a6de62762d7422cbcc1f049fb7147cbba2111333
azim254 <verdixconie@gmail.com>
2020-09-04 08:33:55 +0300

    wrapped check dependecies in a function

--- setup.sh
+++ setup.sh
@@ -23,6 +23,8 @@ general_exitmode_3="Disabling "$grey"forwarding of packets"
 
 
 #checking if all packages are installed & dependecies
+
+function checkdependences {
 REQUIRED_PKG="autossh"
 PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
 echo Checking for $REQUIRED_PKG: $PKG_OK
@@ -30,3 +32,5 @@ if [ "" = "$PKG_OK" ]; then
   echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
   sudo apt-get --yes install $REQUIRED_PKG 
 fi
+
+}
